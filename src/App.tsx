501ff5233cedd945c3f42aa3890ad07d61ff179a
rahajustone <kh.rahmatullo@gmail.com>
2025-06-28 15:12:13 -0400

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-
-import './App.css'
-import { Button } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
+import NavBar from './components/Navbar/NavBar'
 
 function App() {
 
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <Grid templateAreas={
+      { 
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`
+         
+      }}
+    >
+      <GridItem area="nav" padding={2}><NavBar /></GridItem>
+      <GridItem area="aside" bg="blue" display={{ base: "none", lg: "block" }}>Aside</GridItem>
+      <GridItem area="main" bg="green">Main</GridItem>
+    </Grid>
   )
 }
 
