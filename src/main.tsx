a0ecb20ccf4b23a6d2abc4063b2932f11cfeb3f1
rahajustone <kh.rahmatullo@gmail.com>
2025-06-29 19:10:55 -0400

    import reactquery

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { ColorModeProvider } from './components/ui/color-mode'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query' 
 
 import './index.css'
 import App from './App.tsx'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ColorModeProvider>
       <ChakraProvider value={defaultSystem}>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </ChakraProvider>
     </ColorModeProvider>
   </StrictMode>,
